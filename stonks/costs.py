e89f9f1ebd413f799c996884c940203968e8b8af
Lenon <hello@lenon.dev>
2022-01-25 02:23:49 -0300

    Rename functions for brevity

--- stonks/costs.py
+++ stonks/costs.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Calculate volume, costs and net amount since these values are not included in
 # the confirmation (or "nota de corretagem").
-def sum_confirmation_volume_and_costs(confirmations):
+def sum_confirmations_costs(confirmations):
     return confirmations.assign(
         volume=lambda c: c.sells + c.buys,
         costs=lambda c: c.clearing_fees + c.trading_fees + c.brokerage_fees,
@@ -14,7 +14,7 @@ def sum_confirmation_volume_and_costs(confirmations):
 # calculate it for each trade. The "amount" value here is the principal amount,
 # which is price x quantity. For buys it's principal amount + costs, for sells
 # it's principal amount - costs.
-def calc_trade_net_amount(trades):
+def _calc_trade_net_amount(trades):
     return trades.apply(
         lambda t: t.amount + t.costs if t.type == "buy" else t.amount - t.costs,
         axis="columns",
@@ -24,7 +24,7 @@ def calc_trade_net_amount(trades):
 # Calculate amount, costs and net amount for each trade. A single trade has a
 # relationship with only one confirmation entry, but a confirmation could have
 # one or more trades.
-def calc_trade_amount_and_costs(confirmations, trades):
+def calc_trades_costs(confirmations, trades):
     conf_prefix = "c_"
 
     drop_conf_prefix = lambda t: t.drop(
@@ -36,7 +36,7 @@ def calc_trade_amount_and_costs(confirmations, trades):
         .assign(
             amount=lambda t: t.quantity * t.price,
             costs=lambda t: (t.amount / t.c_volume * t.c_costs).round(2),
-            net_amount=calc_trade_net_amount,
+            net_amount=_calc_trade_net_amount,
         )
         .pipe(drop_conf_prefix)
     )
@@ -44,7 +44,7 @@ def calc_trade_amount_and_costs(confirmations, trades):
 
 # Subscriptions net amount is the cost per share x quantity of exercised shares.
 # Costs are already included in cost per share.
-def calc_subscription_net_amount(subscriptions):
+def calc_subscriptions_net_amounts(subscriptions):
     return subscriptions.assign(
         net_amount=subscriptions.exercised * subscriptions.price
     )
