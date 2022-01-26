b9034cf28704fb1945b1b73b04183fbcbe945f37
Lenon Marcel <lenon.marcel@gmail.com>
2022-01-25 23:34:10 -0300

    Remove from conftest fixtures that are required by only a single test

--- tests/test_costs.py
+++ tests/test_costs.py
@@ -5,6 +5,33 @@ from stonks.costs import (
     calc_trades_costs,
     calc_subscriptions_net_amounts,
 )
+from .helpers import fixture_path
+
+
+@fixture
+def confirmations():
+    return pd.read_csv(
+        fixture_path("confirmations.csv"),
+        parse_dates=["date"],
+        index_col=["date", "broker"],
+    )
+
+
+@fixture
+def trades():
+    return pd.read_csv(
+        fixture_path("trades.csv"),
+        parse_dates=["date"],
+        index_col=["date", "broker"],
+    )
+
+
+@fixture
+def subscriptions():
+    return pd.read_csv(
+        fixture_path("subscriptions.csv"),
+        parse_dates=["date", "start", "end", "settlement", "issue_date"],
+    )
 
 
 def test_sum_confirmations_costs(confirmations, confirmations_with_costs):
