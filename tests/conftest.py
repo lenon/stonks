d87f803e555b94df7dfd8ef39590c79bdb25311a
Lenon <hello@lenon.dev>
2022-01-25 23:03:41 -0300

    Add a function to merge all sheets into a dataframe with all events and corporate actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -57,3 +57,35 @@ def subscriptions_with_net_amount():
         fixture_path("subscriptions-with-net-amount.csv"),
         parse_dates=["date", "start", "end", "settlement", "issue_date"],
     )
+
+
+@fixture
+def splits():
+    return pd.read_csv(
+        fixture_path("splits.csv"),
+        parse_dates=["date"],
+    )
+
+
+@fixture
+def mergers():
+    return pd.read_csv(
+        fixture_path("mergers.csv"),
+        parse_dates=["date"],
+    )
+
+
+@fixture
+def spinoffs():
+    return pd.read_csv(
+        fixture_path("spinoffs.csv"),
+        parse_dates=["date"],
+    )
+
+
+@fixture
+def events():
+    return pd.read_csv(
+        fixture_path("events.csv"),
+        parse_dates=["date", "start", "end", "settlement", "issue_date"],
+    )
