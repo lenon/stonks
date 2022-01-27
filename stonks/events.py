305a0da076e716bafad829a515e4fbcd225d0a52
Lenon <hello@lenon.dev>
2022-01-27 03:03:56 -0300

    Add a function to calculate buys

--- stonks/events.py
+++ stonks/events.py
@@ -1,8 +1,28 @@
 import pandas as pd
-from pprint import pprint
 
 
 def concat_dfs(*dfs):
     events = [df.assign(event=event) for event, df in dfs]
 
     return pd.concat(events).sort_values(by="date", ignore_index=True)
+
+
+def buy(positions, event):
+    if event.symbol not in positions.index:
+        # first buy, not yet in positions dataframe
+        new_quantity = event.quantity
+        new_cost = event.net_amount
+        new_cost_per_share = event.net_amount / event.quantity
+    else:
+        # bought it before, let's update cost per share
+        prev = positions.loc[event.symbol]
+
+        new_quantity = prev.quantity + event.quantity
+        new_cost = prev.cost + event.net_amount
+        new_cost_per_share = new_cost / new_quantity
+
+    positions.loc[event.symbol] = {
+        "quantity": new_quantity,
+        "cost": new_cost,
+        "cost_per_share": new_cost_per_share,
+    }
