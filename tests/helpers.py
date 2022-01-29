29c7543e37dc0640b1e60600d96023f5a3d9e450
Lenon Marcel <lenon.marcel@gmail.com>
2022-01-28 22:45:01 -0300

    Move helper functions to helper.py

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,5 +1,18 @@
+import pandas as pd
 from pathlib import Path
 
 
 def fixture_path(fixture):
     return Path(__file__).parent.joinpath("fixtures", fixture).resolve()
+
+
+def make_positions(data):
+    df = pd.DataFrame.from_records(
+        data, index="symbol", columns=["symbol", "quantity", "cost", "cost_per_share"]
+    )
+    df.index.name = None  # reset index name otherwise frame comparison fails
+    return df
+
+
+def make_event(data):
+    return pd.Series(data)
