a0bcaa5cae9e380528ff8428013de34e9c75541c
Lenon <hello@lenon.dev>
2022-01-30 00:58:28 -0300

    Add utility functions to transform column names for the output file

--- stonks/utils.py
+++ stonks/utils.py
@@ -1,5 +1,20 @@
-def ratio_to_float(ratio):
-    # expects ratio to be expressed as A:B and to have comma (,) as decimal separator
-    a, b = [float(term.replace(",", ".")) for term in ratio.split(":")]
+from pathlib import Path
 
+
+# Expects ratio to be expressed as A:B and to have comma (,) as decimal separator.
+def ratio_to_float(ratio):
+    a, b = [float(term.replace(",", ".")) for term in ratio.split(":")]
     return a / b
+
+
+def columns_to_snake_case(name):
+    return name.lower().replace(" ", "_")
+
+
+def columns_to_title_case(name):
+    return name.replace("_", " ").capitalize()
+
+
+def positions_output_path(input_path):
+    input_path = Path(input_path)
+    return input_path.parent.joinpath(f"{input_path.stem}-positions.xlsx")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,10 @@
-from stonks.utils import ratio_to_float
+from pathlib import Path
+from stonks.utils import (
+    ratio_to_float,
+    columns_to_snake_case,
+    columns_to_title_case,
+    positions_output_path,
+)
 
 
 def test_ratio_to_float():
@@ -7,3 +13,17 @@ def test_ratio_to_float():
     assert ratio_to_float("1:4") == 0.25
     assert ratio_to_float("1,25:1") == 1.25
     assert ratio_to_float("1:1,25") == 0.8
+
+
+def test_columns_to_snake_case():
+    assert columns_to_snake_case("My Column") == "my_column"
+    assert columns_to_snake_case("my_column") == "my_column"
+
+
+def test_columns_to_title_case():
+    assert columns_to_title_case("My Column") == "My column"
+    assert columns_to_title_case("my_column") == "My column"
+
+
+def test_positions_output_path():
+    assert positions_output_path("path/to/file.xlsx") == Path("path/to/file-positions.xlsx")
