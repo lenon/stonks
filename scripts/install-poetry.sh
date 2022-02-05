ec26eb86f86a85608162d58e13ddbd4e73ae4887
Lenon <hello@lenon.dev>
2022-02-05 05:40:42 -0300

    Improve ci script

--- scripts/install-poetry.sh
+++ scripts/install-poetry.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-curl --silent \
-    --show-error \
-    --location https://install.python-poetry.org \
-    --output poetry-installer.py
+poetry_dir=~/.local/share/pypoetry
+poetry_exe=~/.local/bin/poetry
 
-python poetry-installer.py --yes --version "$POETRY_VERSION"
+if [[ -d "$poetry_dir" && -x "$poetry_exe" ]]; then
+    # poetry installation folder restored from cache, just add to path
+    echo ~/.local/bin >>"$GITHUB_PATH"
+else
+    curl --silent \
+        --show-error \
+        --location https://install.python-poetry.org \
+        --output poetry-installer.py
+
+    python poetry-installer.py --yes --version "$POETRY_VERSION"
+fi
+
+poetry --version
