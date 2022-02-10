431529d63238a8de3da631f4b42ffb6af30cb355
Lenon Marcel <lenon.marcel@gmail.com>
2022-02-10 03:18:06 -0300

    Small refactoring

--- stonks/costs.py
+++ stonks/costs.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 # Calculate volume (sells + buys), costs (sum of all fees) and net amount (sells
 # - buys - costs). These columns are not included in trade confirmations ("nota
 # de corretagem" in Portuguese) and are required for other calculations.
@@ -24,18 +25,23 @@ def _calc_trade_net_amount(trades):
 # costs) and net amount for each trade. A trade confirmation has a one-to-many
 # association with a trade, identified by date and broker name.
 def calc_trades_costs(trades, confirmations):
-    conf_prefix = "c_"
+    confirmations_prefix = "c_"
+    confirmations_with_prefix = confirmations.add_prefix(confirmations_prefix)
 
-    drop_conf_prefix = lambda t: t.drop(columns=t.columns[t.columns.str.startswith(conf_prefix)])
+    drop_confirmations_prefix = lambda t: t.drop(
+        columns=t.columns[t.columns.str.startswith(confirmations_prefix)]
+    )
 
     return (
-        trades.join(confirmations.add_prefix(conf_prefix), on=["date", "broker"])
+        # join confirmations columns with a prefix to avoid overriding values
+        # for columns with the same name
+        trades.join(confirmations_with_prefix, on=["date", "broker"])
         .assign(
             amount=lambda t: t.quantity * t.price,
             costs=lambda t: (t.amount / t.c_volume * t.c_costs).round(2),
             net_amount=_calc_trade_net_amount,
         )
-        .pipe(drop_conf_prefix)
+        .pipe(drop_confirmations_prefix)
     )
 
 
