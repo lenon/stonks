0c1024103d932324f654b84479b7d7939d42f0e6
Lenon Marcel <lenon.marcel@gmail.com>
2022-04-05 15:06:43 -0300

    Reimplement costs calculations in a way that can be used with xlwings
    Instead of returning existing columns merged with calculated columns,
    return just the calculated columns. This is better for debugging and
    should be easier to use with xlwings.

--- stonks/costs.py
+++ stonks/costs.py
@@ -1,48 +1,40 @@
-# Calculate volume (sells + buys), costs (sum of all fees) and net amount (sells
-# - buys - costs). These columns are not included in trade confirmations ("nota
-# de corretagem" in Portuguese) and are required for other calculations.
+import numpy as np
+import pandas as pd
+
+
+# Calculate some columns that are not included in trade confirmations ("nota de
+# corretagem" in Portuguese) and are required for other calculations.
 def sum_confirmations_costs(confirmations):
-    return confirmations.assign(
-        volume=lambda c: c.sells + c.buys,
-        costs=lambda c: c.clearing_fees + c.trading_fees + c.brokerage_fees,
-        net_amount=lambda c: c.sells - c.buys - c.costs,
+    volume = confirmations.sells + confirmations.buys
+    costs = confirmations.clearing_fees + confirmations.trading_fees + confirmations.brokerage_fees
+    net_amount = confirmations.sells - confirmations.buys - costs
+
+    return pd.concat(
+        [volume, costs, net_amount], axis="columns", keys=["volume", "costs", "net_amount"]
     )
 
 
-# Calculate net amount for each trade, which is principal amount + costs for
-# buys and principal amount - costs for sells.
-def _calc_trade_net_amount(trades):
-    return trades.apply(
-        lambda t: t.amount + t.costs if t.type == "buy" else t.amount - t.costs,
-        axis="columns",
-    )
-
-
-# Calculate amount (quantity x price), pro rata costs (amount / volume * total
-# costs) and net amount for each trade. A trade confirmation has a one-to-many
-# association with a trade, identified by date and broker name.
+# Calculate pro rata costs for each trade. This is also not included in trade
+# confirmations and is required to calculate the portfolio.
 def calc_trades_costs(trades, confirmations):
-    confirmations_prefix = "c_"
-    confirmations_with_prefix = confirmations.add_prefix(confirmations_prefix)
+    # trade confirmation has a one-to-many association with a trade, identified
+    # by date and broker name
+    trades_w_confirmations = trades.join(confirmations, on=["date", "broker"], rsuffix="_c")
 
-    drop_confirmations_prefix = lambda t: t.drop(
-        columns=t.columns[t.columns.str.startswith(confirmations_prefix)]
-    )
+    amount = trades_w_confirmations.quantity * trades_w_confirmations.price
+    costs = (amount / trades_w_confirmations.volume * trades_w_confirmations.costs_c).round(2)
 
-    return (
-        # join confirmations columns with a prefix to avoid overriding values
-        # for columns with the same name
-        trades.join(confirmations_with_prefix, on=["date", "broker"])
-        .assign(
-            amount=lambda t: t.quantity * t.price,
-            costs=lambda t: (t.amount / t.c_volume * t.c_costs).round(2),
-            net_amount=_calc_trade_net_amount,
-        )
-        .pipe(drop_confirmations_prefix)
+    # net amount for buys is principal amount + costs, while for sells it's
+    # amount - costs
+    costs_with_sign = costs * np.where(trades_w_confirmations.type == "buy", 1, -1)
+    net_amount = amount + costs_with_sign
+
+    return pd.concat(
+        [amount, costs, net_amount], axis="columns", keys=["amount", "costs", "net_amount"]
     )
 
 
 # Subscriptions net amount is the cost per share x quantity of exercised shares.
 # Costs are already included in cost per share.
 def calc_subscriptions_net_amounts(subscriptions):
-    return subscriptions.assign(net_amount=subscriptions.exercised * subscriptions.price)
+    return pd.DataFrame().assign(net_amount=subscriptions.exercised * subscriptions.price)
