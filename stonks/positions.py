7b90ac4653085284fcacffe5659442eaf6264182
Lenon Marcel <lenon.marcel@gmail.com>
2022-04-06 13:50:35 -0300

    Perform schema validations before calculating positions

--- stonks/positions.py
+++ stonks/positions.py
@@ -1,14 +1,27 @@
 import pandas as pd
 from .events import event_fn, concat_events
+from .schemas import (
+    SplitSchema,
+    TradeSchema,
+    MergerSchema,
+    SpinoffSchema,
+    SubscriptionSchema,
+)
 
 
 def calc_positions(trades, subscriptions, splits, mergers, spinoffs):
+    trades_df = TradeSchema(trades)
+    subscriptions_df = SubscriptionSchema(subscriptions)
+    splits_df = SplitSchema(splits)
+    mergers_df = MergerSchema(mergers)
+    spinoffs_df = SpinoffSchema(spinoffs)
+
     events = concat_events(
-        ["trade", trades.reset_index()],
-        ["subscription", subscriptions],
-        ["split", splits],
-        ["merger", mergers],
-        ["spinoff", spinoffs],
+        ["trade", trades_df.reset_index()],
+        ["subscription", subscriptions_df],
+        ["split", splits_df],
+        ["merger", mergers_df],
+        ["spinoff", spinoffs_df],
     )
 
     positions = pd.DataFrame(columns=["quantity", "cost", "cost_per_share"])
---
+++ stonks/schemas.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from pandera import Check, Index, Column, Timestamp, MultiIndex, DataFrameSchema
+from stonks.events import spinoff
+
+ConfirmationSchema = DataFrameSchema(
+    columns={
+        "sells": Column(float, Check.ge(0)),
+        "buys": Column(float, Check.ge(0)),
+        "volume": Column(float, Check.ge(0)),
+        "clearing_fees": Column(float, Check.ge(0)),
+        "trading_fees": Column(float, Check.ge(0)),
+        "brokerage_fees": Column(float, Check.ge(0)),
+        "income_tax": Column(float, Check.ge(0)),
+        "costs": Column(float, Check.ge(0)),
+        "net_amount": Column(float),
+    },
+    index=MultiIndex([Index(Timestamp, name="date"), Index(str, name="broker")]),
+    strict=True,
+)
+
+TradeSchema = DataFrameSchema(
+    columns={
+        "symbol": Column(str),
+        "type": Column(str, Check.isin(["buy", "sell"])),
+        "quantity": Column(float, Check.gt(0)),
+        "price": Column(float, Check.ge(0)),
+        "amount": Column(float, Check.ge(0)),
+        "costs": Column(float, Check.ge(0)),
+        "net_amount": Column(float),
+    },
+    index=MultiIndex([Index(Timestamp, name="date"), Index(str, name="broker")]),
+    strict=True,
+)
+
+SubscriptionSchema = DataFrameSchema(
+    columns={
+        "date": Column(Timestamp),
+        "broker": Column(str),
+        "symbol": Column(str),
+        "description": Column(str),
+        "start": Column(Timestamp),
+        "end": Column(Timestamp),
+        "settlement": Column(Timestamp),
+        "shares": Column(float, Check.gt(0)),
+        "exercised": Column(float, Check.gt(0)),
+        "price": Column(float, Check.gt(0)),
+        "net_amount": Column(float, Check.gt(0)),
+        "issue_date": Column(Timestamp, nullable=True),
+    },
+    strict=True,
+)
+
+SplitSchema = DataFrameSchema(
+    columns={
+        "date": Column(Timestamp),
+        "symbol": Column(str),
+        "ratio": Column(str, Check.str_matches(r"^[\d,]+:[\d]+$")),
+    },
+    strict=True,
+)
+
+MergerSchema = DataFrameSchema(
+    columns={
+        "date": Column(Timestamp),
+        "symbol": Column(str),
+        "acquirer": Column(str),
+        "ratio": Column(str, Check.str_matches(r"^[\d,]+:[\d]+$")),
+    },
+    strict=True,
+)
+
+SpinoffSchema = DataFrameSchema(
+    columns={
+        "date": Column(Timestamp),
+        "symbol": Column(str),
+        "new_company": Column(str),
+        "ratio": Column(str, Check.str_matches(r"^[\d,]+:[\d]+$")),
+        "cost_basis": Column(float, Check.gt(0)),
+    },
+    strict=True,
+)
