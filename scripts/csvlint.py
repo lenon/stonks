3ef461f70d279da31a7c0c714ae8a1f16a8302d1
Lenon <hello@lenon.dev>
2023-03-05 15:27:42 -0300

    Add a script to lint csv files generated by apple numbers

---
+++ scripts/csvlint.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import os
+import csv
+import sys
+
+if len(sys.argv) < 2:
+    print("error: must pass a file", file=sys.stderr)
+    exit(1)
+
+file = sys.argv[1]
+tmpfile = f"{file}.tmp"
+
+if not file or not os.path.isfile(file) or not os.access(file, os.R_OK):
+    print(f"error: file {file} is not a readable file", file=sys.stderr)
+    exit(1)
+
+
+with open(file, "r", newline="") as input, open(tmpfile, "w", newline="") as output:
+    reader = csv.reader(input, delimiter=";")
+    writer = csv.writer(output, delimiter=",", lineterminator="\n")
+
+    writer.writerows(reader)
+
+os.replace(tmpfile, file)
