12ed9611ce0230e93e18b0ac04af7cf33f53ae6d
Lenon <hello@lenon.dev>
2023-03-07 00:09:48 -0300

    Encapsulate positions df into a class

--- stonks/positions.py
+++ stonks/positions.py
@@ -3,6 +3,37 @@ from .events import event_fn, concat_events, filter_by_date
 from .schemas import Rights, Splits, Trades, Mergers, SpinOffs, StockDividends
 
 
+class Positions:
+    def __init__(self):
+        self._df = pd.DataFrame(columns=["quantity", "cost", "cost_per_share"]).rename_axis(
+            "symbol"
+        )
+
+    def is_open(self, symbol):
+        return symbol in self._df.index
+
+    def is_closed(self, symbol):
+        return not self.is_open(symbol)
+
+    def find(self, symbol):
+        return self._df.loc[symbol]
+
+    def update(self, symbol, quantity, cost, cost_per_share):
+        self._df.loc[symbol] = {
+            "quantity": quantity,
+            "cost": cost,
+            "cost_per_share": cost_per_share,
+        }
+
+    def close(self, symbol):
+        self._df.drop(labels=symbol, inplace=True)
+
+    def to_df(self):
+        # return a dataframe sorted by symbol, with 2 decimal places and a
+        # sequential numeric index
+        return self._df.sort_index().round(2).reset_index()
+
+
 def calc_positions(date, trades, rights, splits, mergers, spin_offs, stock_dividends):
     trades_df = Trades(trades)
     rights_df = Rights(rights)
@@ -20,14 +51,10 @@ def calc_positions(date, trades, rights, splits, mergers, spin_offs, stock_divid
         ["stock_dividend", stock_dividends_df],
     )
     filtered_events = filter_by_date(events=events, date=date)
-    positions = pd.DataFrame(columns=["quantity", "cost", "cost_per_share"])
+    positions = Positions()
 
     for _, event in filtered_events.iterrows():
         fn = event_fn(event)
         fn(positions, event)
 
-    sorted_positions = (
-        positions.sort_index().round(2).reset_index().rename(columns={"index": "symbol"})
-    )
-
-    return sorted_positions
+    return positions.to_df()
