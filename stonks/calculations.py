8dba5717069f9e3e84fdd26d2a3f9d1f22e1eb51
Lenon <hello@lenon.dev>
2023-03-11 13:23:09 -0300

    Check inputs on calc_trades_costs

--- stonks/calculations.py
+++ stonks/calculations.py
@@ -28,11 +28,16 @@ def calc_trade_confirmations_costs(trade_confirmations: pd.DataFrame):
     )
 
 
-# Calculate pro rata costs for each trade. This is also not included in trade
-# confirmations and is required to calculate the portfolio.
-def calc_trades_costs(trades, trade_confirmations):
-    # trade confirmation has a one-to-many association with a trade, identified
-    # by date and broker name
+# Calculate pro rata costs for each trade included in a trade confirmation.
+#
+# Amount: is the quantity of the traded security x price.
+# Costs: pro rata costs based on the amount of the traded security.
+# Net amount: the amount of traded security + any costs.
+@check_input(Trades, 0)
+@check_input(TradeConfirmations, 1)
+def calc_trades_costs(trades: pd.DataFrame, trade_confirmations: pd.DataFrame):
+    # The trade confirmation has a one-to-many association with trades, meaning
+    # that a single trade confirmation has one or more trades.
     trades_w_confirmations = trades.join(trade_confirmations, on=["date", "broker"], rsuffix="_c")
 
     amount = trades_w_confirmations.quantity * trades_w_confirmations.price
