28e905668c053ec31cd358ecb81d50cf60ac98dc
Lenon <hello@lenon.dev>
2023-03-12 14:21:56 -0300

    Move calc_positions to calculations.py

--- stonks/calculations.py
+++ stonks/calculations.py
@@ -1,7 +1,18 @@
 import numpy as np
 import pandas as pd
 from pandera import check_input
-from .schemas import Rights, Trades, TradeConfirmations, TradeConfirmationsWithNullable
+from .events import event_fn, concat_events, filter_by_date
+from .schemas import (
+    Rights,
+    Splits,
+    Trades,
+    Mergers,
+    SpinOffs,
+    StockDividends,
+    TradeConfirmations,
+    TradeConfirmationsWithNullable,
+)
+from .positions import Positions
 
 
 # Trade confirmation is a document that confirms the details of a trade, such as
@@ -61,3 +72,29 @@ def calc_trades_costs(trades: pd.DataFrame, trade_confirmations: pd.DataFrame):
 def calc_rights_net_amounts(rights: pd.DataFrame):
     net_amount = rights.exercised * rights.price
     return net_amount.to_frame(name="net_amount")
+
+
+def calc_positions(date, trades, rights, splits, mergers, spin_offs, stock_dividends):
+    trades_df = Trades(trades)
+    rights_df = Rights(rights)
+    splits_df = Splits(splits)
+    mergers_df = Mergers(mergers)
+    spin_offs_df = SpinOffs(spin_offs)
+    stock_dividends_df = StockDividends(stock_dividends)
+
+    events = concat_events(
+        ["trade", trades_df.reset_index()],
+        ["right", rights_df.reset_index()],
+        ["split", splits_df.reset_index()],
+        ["merger", mergers_df.reset_index()],
+        ["spin_off", spin_offs_df.reset_index()],
+        ["stock_dividend", stock_dividends_df.reset_index()],
+    )
+    filtered_events = filter_by_date(events=events, date=date)
+    positions = Positions()
+
+    for _, event in filtered_events.iterrows():
+        fn = event_fn(event)
+        fn(positions, event)
+
+    return positions.to_df()
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,7 @@
-import pandas as pd
+from datetime import date
+from pandas.testing import assert_frame_equal
 from stonks.calculations import (
+    calc_positions,
     calc_trades_costs,
     calc_rights_net_amounts,
     calc_trade_confirmations_costs,
@@ -9,16 +11,38 @@ from stonks.calculations import (
 def test_calc_trade_confirmations_costs(trade_confirmations_df, trade_confirmations_costs_df):
     results = calc_trade_confirmations_costs(trade_confirmations_df)
 
-    pd.testing.assert_frame_equal(results, trade_confirmations_costs_df)
+    assert_frame_equal(results, trade_confirmations_costs_df)
 
 
 def test_calc_trades_costs(trade_confirmations_with_costs_df, trades_df, trades_costs_df):
     results = calc_trades_costs(trades_df, trade_confirmations_with_costs_df)
 
-    pd.testing.assert_frame_equal(results, trades_costs_df)
+    assert_frame_equal(results, trades_costs_df)
 
 
 def test_calc_rights_net_amounts(rights_df, rights_amounts_df):
     results = calc_rights_net_amounts(rights_df)
 
-    pd.testing.assert_frame_equal(results, rights_amounts_df)
+    assert_frame_equal(results, rights_amounts_df)
+
+
+def test_calc_positions(
+    positions_df,
+    trades_with_costs_df,
+    rights_with_amounts_df,
+    splits_df,
+    mergers_df,
+    spin_offs_df,
+    stock_dividends_df,
+):
+    actual_positions = calc_positions(
+        date=date.today(),
+        trades=trades_with_costs_df,
+        rights=rights_with_amounts_df,
+        splits=splits_df,
+        mergers=mergers_df,
+        spin_offs=spin_offs_df,
+        stock_dividends=stock_dividends_df,
+    )
+
+    assert_frame_equal(actual_positions, positions_df)
