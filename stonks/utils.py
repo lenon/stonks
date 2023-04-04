dc40138e04de73a4446b08d4514a75ed64e39882
Lenon <hello@lenon.dev>
2023-04-03 23:49:08 -0300

    Add a function to calculate 15th day of the previous month

--- stonks/utils.py
+++ stonks/utils.py
@@ -1,3 +1,6 @@
+from datetime import date, timedelta
+
+
 # Expects ratio to be expressed as A:B and to have comma (,) as decimal separator.
 def ratio_to_float(ratio: str) -> float:
     a, b = [float(term.replace(",", ".")) for term in ratio.split(":")]
@@ -6,3 +9,14 @@ def ratio_to_float(ratio: str) -> float:
 
 def reverse_dict(d: dict[str, str]) -> dict[str, str]:
     return {v: k for k, v in d.items()}
+
+
+def previous_month_15th(date: date) -> date:
+    # change the date to 1st of the month then subtract 1 day and we get the
+    # previous month
+    previous_month = date.replace(day=1) - timedelta(days=1)
+
+    # calculate the 15th day of the previous month
+    previous_month_15th = previous_month.replace(day=15)
+
+    return previous_month_15th
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
-from stonks.utils import reverse_dict, ratio_to_float
+from datetime import date
+from stonks.utils import reverse_dict, ratio_to_float, previous_month_15th
 
 
 def test_ratio_to_float():
@@ -11,3 +12,11 @@ def test_ratio_to_float():
 
 def test_reverse_dict():
     assert reverse_dict({"a": "b", "c": "d"}) == {"b": "a", "d": "c"}
+
+
+def test_previous_month_15th():
+    assert previous_month_15th(date(2022, 1, 1)) == date(2021, 12, 15)
+    assert previous_month_15th(date(2022, 1, 15)) == date(2021, 12, 15)
+    assert previous_month_15th(date(2022, 1, 31)) == date(2021, 12, 15)
+    assert previous_month_15th(date(2022, 2, 28)) == date(2022, 1, 15)
+    assert previous_month_15th(date(2022, 12, 31)) == date(2022, 11, 15)
