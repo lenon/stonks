92f4bd0ce4083fff1ab90e204c8eef49b848db54
Lenon <hello@lenon.dev>
2024-03-24 22:53:20 -0300

    Reimplement positions with a simple dict instead of a df

--- stonks/positions.py
+++ stonks/positions.py
@@ -1,31 +1,46 @@
-from pandas import DataFrame, Series
+from dataclasses import dataclass
+
+from pandas import DataFrame
+
+
+@dataclass
+class Position:
+    quantity: float
+    cost: float
+    cost_per_share: float
 
 
 class Positions:
     def __init__(self) -> None:
-        self._df = DataFrame(columns=["quantity", "cost", "cost_per_share"]).rename_axis("symbol")
+        self._positions: dict[str, Position] = {}
 
     def is_open(self, symbol: str) -> bool:
-        return symbol in self._df.index
+        return symbol in self._positions
 
     def is_closed(self, symbol: str) -> bool:
         return not self.is_open(symbol)
 
-    def find(self, symbol: str) -> Series:
-        position: Series = self._df.loc[symbol]
-        return position
+    def find(self, symbol: str) -> Position:
+        return self._positions[symbol]
 
-    def update(self, symbol: str, quantity: float, cost: float, cost_per_share: float) -> None:
-        self._df.loc[symbol] = {
-            "quantity": quantity,
-            "cost": cost,
-            "cost_per_share": cost_per_share,
-        }
+    def update(self, symbol: str, /, quantity: float, cost: float, cost_per_share: float) -> None:
+        self._positions[symbol] = Position(
+            quantity=quantity,
+            cost=cost,
+            cost_per_share=cost_per_share,
+        )
 
     def close(self, symbol: str) -> None:
-        self._df.drop(labels=symbol, inplace=True)
+        self._positions.pop(symbol)
 
     def to_df(self) -> DataFrame:
-        # return a dataframe sorted by symbol
-        # round cost and cost per share, keep quantity as is
-        return self._df.sort_index().round({"cost": 2, "cost_per_share": 2})
+        return (
+            DataFrame.from_dict(
+                self._positions,
+                orient="index",
+                columns=["quantity", "cost", "cost_per_share"],
+            )
+            .rename_axis("symbol")  # rename the index
+            .sort_index()  # sort by symbol
+            .round({"cost": 2, "cost_per_share": 2})  # round costs, keep quantity as is
+        )
